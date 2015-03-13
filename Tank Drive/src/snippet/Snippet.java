ee94288b1d3676f5bf20a5aee3c27347b03268c5
Tree-plant <Ol-1utio1>
2015-03-13 16:43:32 -0400

    Setting up repository

---
+++ Tank Drive/src/snippet/Snippet.java
@@ -0,0 +1,11 @@
+package snippet;
+
+public class Snippet {
+	echo # Season2015 >> README.md
+	git init
+	git add README.md
+	git commit -m "first commit"
+	git remote add origin https://github.com/PHScodemaster/Season2015.git
+	git push -u origin master
+}
+
